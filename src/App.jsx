e50fe3397a226ebeb0238d2701975c901a24217e
Souvik Ghosh <mesouvikghosh96@gmail.com>
2025-01-05 11:05:59 +0530

    change UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPasword] = useState('')
 
   return (
-    <>
-     
-    </>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg
+    px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-3xl text-center font-bold mb-2'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3' placeholder='Password' readOnly
+        />
+        <button className='text-white bg-green-500 px-3 py-0.5 outline-none shrink-0'>Copy</button>
+      </div>
+      <div className='flex items-center gap-x-1'>
+        <input type="range"
+          min={8}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e) => setLength(e.target.value)}
+          name=''
+          id=''
+        />
+        <label htmlFor="length">Length: {length}</label>
+
+        <div className='flex items-center gap-x-1'>
+          <input type="checkbox"
+            defaultChecked={numberAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev)
+            }}
+            name=''
+            id=''
+          />
+          <label htmlFor="number">Numbers</label>
+
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox"
+              defaultChecked={numberAllowed}
+              onChange={() => {
+                charAllowed((prev) => !prev)
+              }}
+              name=''
+              id=''
+            />
+            <label htmlFor="number">Charecter</label>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
